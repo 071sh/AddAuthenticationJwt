a73ec9c5a7deca1ca7438239cabfd5ca081ae013
shayan safaei <70308725+071sh@users.noreply.github.com>
2021-11-30 12:16:40 +0330

    Add files via upload

---
+++ AppSettingService.cs
@@ -0,0 +1,39 @@
+﻿using Application.Dto;
+using Application.Dto.Setting;
+using Application.Interface.Setting;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Service.Setting
+{
+    public class AppSettingService : IAppSettingService
+    {
+        private readonly IDataBaseContext _context;
+        private readonly IMapper _mapper;
+
+        public AppSettingService(IDataBaseContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public BaseDto<SettingDto.AppSettingDto> Excecute(string key)
+        {
+            var data = _context.AppSettings.FirstOrDefault(x => x.Key == key);
+            if (data == null)
+                return new BaseDto<SettingDto.AppSettingDto>(false, new List<string> { "Not Found" },null);
+
+            var result= _mapper.Map<SettingDto.AppSettingDto>(data);
+
+            return new BaseDto<SettingDto.AppSettingDto>(true, new List<string> { "Done" }, result);
+        }
+        public string GetValue(string key)
+        {
+            return _context.AppSettings.First(x => x.Key == key).Value;
+        }
+    }
+}
